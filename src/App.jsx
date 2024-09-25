9c1367ae9d13b79e286fd67b65cd8ab02c6d4593
fkilld <fkill.d.azadd@gmail.com>
2024-09-25 21:58:41 +0530

    2nd day

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
-
 import Header from './components/Header'
 import AddTask from './components/AddTask'
 import ShowTask from './components/ShowTask'
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState()
-  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem('tasklist')) || [])
+  const [task, setTask] = useState()
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem('tasklist')) || []
+  )
   const [editid, setEditid] = useState(0)
-  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'medium')
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem('theme')) || 'medium'
+  )
 
-  const HandleSubmit = event => {}
-  const handleEdit = id => {}
-  const handleDelete = (id) => {}
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (editid) {
+      const date = new Date()
+      const selectedTask = taskList.find((task) => task.id === editid)
+      const updatedTask = taskList.map((e) => {
+        e.id === selectedTask.id
+          ? {
+              id: e.id,
+              name: task,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+            }
+          : { id: e.id, name: e.name, time: e.time }
+      })
+      setTaskList(updatedTask)
+      setTask('')
+      setEditid(0)
+      return
+    }
+    if (task) {
+      const date = new Date()
+      setTaskList([
+        ...taskList,
+        {
+          id: date.getTime(),
+          name: task,
+          time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+        },
+      ])
+      setTask('')
+    }
+  }
+  const handleEdit = (id) => {
+    const selectedTask = taskList.find((task) => task.id === id)
+    setTask(selectedTask.name)
+    setEditid(id)
+  }
+  const handleDelete = (id) => {
+    const updatedTask = taskList.filter((task) => task.id !== id)
+    setTaskList(updatedTask)
+  }
   useEffect(() => {
     localStorage.setItem('tasklist', JSON.stringify(taskList))
   }, [taskList])
@@ -24,13 +65,25 @@ function App() {
       className={`App min-h-screen flex flex-col justify-center items-center bg-gray-50 ${theme}`}
     >
       <div className='container max-w-2xl mx-auto py-10 px-6 shadow-lg bg-white rounded-lg'>
-        <Header setTheme={setTheme} theme={theme} >
+        <Header setTheme={setTheme} theme={theme}>
           LearnWithAzad
         </Header>
       </div>
 
-      <AddTask />
-      <ShowTask />
+      <div className='my-8'>
+        <AddTask
+          handleSubmit={handleSubmit}
+          editid={editid}
+          task={task}
+          setTask={setTask}
+        />
+        <ShowTask
+          taskList={taskList}
+          setTaskList={setTaskList}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      </div>
     </div>
   )
 }
--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 
-function ShowTask() {
+function ShowTask({ taskList, setTaskList, handleEdit, handleDelete }) {
+  const [deletingTask, setDeletingTask] = useState(false)
+  const handleDeleteWithAnimation = (id) => {}
+
   return (
-    <div>ShowTask</div>
+    <div className='w-full'>
+      <div className='flex justify-between items-center mb-4'>
+        <h2 className='text-2xl font-bold text-indigo-600'>
+          tasks ({taskList.length})
+        </h2>
+        <button
+          className='text-red-600 hover:text-red-700 font-semibold'
+          onClick={() => setTaskList([])}
+        >
+          clear all
+        </button>
+      </div>
+      <ul className='space-y-4'>
+        <li className={`flex justify-between items-center p-4 bg-gray-100 transition-all duration-500 rounded-lg shadow-md `}>1</li>
+        <li>2</li>
+      </ul>
+    </div>
   )
 }
 
